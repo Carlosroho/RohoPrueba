0783e27ed5763843abc6ebf501dbb25fb04d1825
Carlos <luis@roho.fitness>
2018-09-03 11:58:55 -0600

    Branch test

--- app/views/master.blade.php
+++ app/views/master.blade.php
@@ -6,7 +6,7 @@
             <ul class="sidebar-nav">
                 <li class="sidebar-brand">
                     <a href="#">
-                        Start Bootstrap
+                        Panda Bar
                     </a>
                 </li>
                 <li>
@@ -39,7 +39,7 @@
             <div class="container-fluid">
                 <div class="row">
                     <div class="col-lg-12">
-                        <h1>Simple Sidebar</h1>
+                        <h1>Barra Simple</h1>
                         <p>This template has a responsive menu toggling system. The menu will appear collapsed on smaller screens, and will appear non-collapsed on larger screens. When toggled using the button below, the menu will appear/disappear. On small screens, the page content will be pushed off canvas.</p>
                         <p>Make sure to keep all page content within the <code>#page-content-wrapper</code>.</p>
                         <a href="#menu-toggle" class="btn btn-default" id="menu-toggle">Toggle Menu</a>
